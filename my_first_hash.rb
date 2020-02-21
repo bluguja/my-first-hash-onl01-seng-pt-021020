82a30463b67342387d8449e59914bb70bb744a69
Bala Luguja <bluguja@gmail.com>
2020-02-21 16:06:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash={"name" => "Boby", "Age" => 24 }
+  my_hash={"Name" => "Bob", "Age" => 34}
   
 end
 
